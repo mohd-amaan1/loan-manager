2bd3bf64ed0c4753798ff4fb39f887e6e8c99588
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2025-03-30 00:45:35 +0530

    added navbar sidebar cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+"use client";
+
+import DashboardCards from "@/components/cards";
+import NavbarA from "@/components/navBarA";
+import Sidebar from "@/components/sideBar";
+import { useState } from "react";
 
 export default function Home() {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen((prev) => !prev);
+  };
+
   return (
-    <div>
-      Amaan
+    <div className="flex min-h-screen">
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+      <div className="flex-1 flex flex-col">
+        <NavbarA toggleSidebar={toggleSidebar} />
+        
+        <div className="flex-1 p-6 bg-gray-100">
+          <DashboardCards />
+        </div>
+      </div>
     </div>
   );
 }
